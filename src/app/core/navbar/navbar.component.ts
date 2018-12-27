9f4b3bbcb3d84ef6e4cd6c0e4b57bec03af915d7
TuanKhoi <17520648@gm.uit.edu.vn>
2018-12-27 18:09:37 +0700

    commit

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  isShow = true;
+  currentShow: any;
+  showNavbar() {
+    this.currentShow = { active: this.isShow};
+    this.isShow = !this.isShow;
   }
+
+  constructor() {
+
+  }
+  ngOnInit() { }
 
 }
